71126e66670a13ae6c04b85347a3d15ebde014ad
akioUnity <akioUnity@gmail.com>
2019-10-20 21:22:26 +0700

    Lamigame finish dlg 60%

---
+++ Assets/RummyScript/LamiGame/UI/UILamiFinish.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILamiFinish : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnReportBtn()
+    {
+        
+    }
+    
+    public void OnExitBtn()
+    {
+        
+    }
+    
+    public void OnContinueBtn()
+    {
+        
+    }
+}
