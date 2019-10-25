326fd2280a0b1e0603368175eb1b67ab77b323e1
akioUnity <akioUnity@gmail.com>
2019-10-26 06:25:43 +0700

    add other files

---
+++ Assets/RummyScript/LamiGame/UI/UILamiCardSelect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILamiCardSelect : MonoBehaviour
+{
+    public int id;
+    // Start is called before the first frame update
+    
+    public void OnClickList()
+    {
+        LamiGameUIManager.Inst.OnSelectedCardList(id);
+        LamiGameUIManager.Inst.uiSelectCardList.Hide();
+    }
+
+}
---
+++ Assets/RummyScript/LamiGame/UI/UILamiSelectCardList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILamiSelectCardList : MonoBehaviour
+{
+    public UILamiCardSelect listObj;
+    public LamiGameCard gameCard;
+    public List<List<Card>> mList;
+    public void Show(List<List<Card>> list)
+    {
+        transform.parent.gameObject.SetActive(true);
+        mList = list;
+        for (int i = 0; i < mList.Count; i ++)
+        {
+            UILamiCardSelect cardSelect = Instantiate(listObj,transform);
+            cardSelect.id = i;
+            foreach (var card in list[i])
+            {
+                LamiGameCard mCard = Instantiate(gameCard,cardSelect.transform);
+                mCard.UpdateCard(card);
+            }
+        }
+    }
+
+    public void Hide()
+    {
+        foreach (Transform child in transform) {
+            GameObject.Destroy(child.gameObject);
+        }
+        transform.parent.gameObject.SetActive(false);
+    }
+}
