f54b87b5059ee0864ad57b5a88f23f7c2e091de5
akioUnity <akioUnity@gmail.com>
2019-11-02 03:44:20 +0700

    add history

---
+++ Assets/RummyScript/Baccarat/UI/UIBHistory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBHistory : MonoBehaviour
+{
+    public static UIBHistory Inst;
+    public GameObject layout;
+    public UIBHistoryCell[] cells;
+    public int[] typeCnt;
+    public int num;
+    private bool isShow = false;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (!Inst)
+            Inst = this;
+        else
+        {
+            Destroy	(this.gameObject);
+        }
+    }
+
+    void Start()
+    {
+        cells=layout.GetComponentsInChildren<UIBHistoryCell>();
+    }
+
+    public void OnClickShowBtn()
+    {
+        isShow = !isShow;
+        layout.transform.parent.gameObject.SetActive(isShow);
+    }
+
+    public void AddCell(int type)
+    {
+        cells[num].UpdateInfo(type);
+        typeCnt[type]++;
+        num++;
+    }
+}
---
+++ Assets/RummyScript/Baccarat/UI/UIBHistoryCell.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBHistoryCell : MonoBehaviour
+{
+    public Image circle;
+    public Text letter;
+    public int id;
+    public int type;
+    
+    void Start()
+    {
+        
+    }
+
+    public void UpdateInfo(int type)
+    {
+        this.type = type;
+        if (type == 0)
+        {
+            circle.color = new Color(66, 135, 39);
+            letter.text = "T";
+        }
+        else if (type == 1)
+        {
+            circle.color = new Color(236, 50, 63);
+            letter.text = "B";
+        }
+        else if (type == 2)
+        {
+            circle.color = new Color(50, 101, 236);
+            letter.text = "P";
+        }
+
+        circle.gameObject.SetActive(true);
+        letter.gameObject.SetActive(true);
+    }
+}
