d72502cb51700f16eaba60ddcd97a21636cddaca
blockfan@outlook.com <blockfan@outlook.com>
2019-11-01 17:56:24 +0800

    WOrking on baccarat management.

---
+++ Assets/RummyScript/Baccarat/UI/BaccaratRoomController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BaccaratRoomController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static BaccaratRoomController Inst;
+    public existingRoomPanelController existingRoomPanel;
+    public newRoomPanelController newRoomPanel;
+
+    void Awake()
+    {
+        try
+        {
+            UIController.Inst.loadingDlg.gameObject.SetActive(false);
+        }
+        catch { }
+        if (!DataController.Inst)
+            SceneManager.LoadScene("2_Lobby");
+    }
+    void Start()
+    {
+        if (!Inst)
+            Inst = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickCreateRoomDialog()
+    {
+        existingRoomPanel.gameObject.SetActive(false);
+        newRoomPanel.gameObject.SetActive(true);
+    }
+    public void OnClickRoomViewDialog(int id)
+    {
+        existingRoomPanel.gameObject.SetActive(true);
+        newRoomPanel.gameObject.SetActive(false);
+
+        ShowRoomList(id);
+    }
+
+    private void ShowRoomList(int id)
+    {
+        //existingRoomPanel
+        existingRoomPanel.ShowRoomList(id);
+    }
+}
+
+public class BaccaratRoomInfo
+{
+    public string tableName;
+    public bool isPrivate;
+    public int minBet;
+    public int maxBet;
+    public string status;
+    public int playersNum;
+    public int totalPlayers;
+    public int roomNo;
+    public string password;
+    public int roomType;
+    public string roomString
+    {
+        get
+        {
+            return string.Format("{0}:{1}:{2}:{3}:{4}:{5}:{6}:{7}:{8}:{9}", tableName, isPrivate, minBet, maxBet, status, playersNum, totalPlayers, roomNo, password, roomType);
+        }
+        set
+        {
+            var list = value.Split(':');
+            tableName = list[0];
+            isPrivate = bool.Parse(list[1]);
+            minBet = int.Parse(list[2]);
+            maxBet = int.Parse(list[3]);
+            status = list[4];
+            playersNum = int.Parse(list[5]);
+            totalPlayers = int.Parse(list[6]);
+            roomNo = int.Parse(list[7]);
+            password = list[8];
+            roomType = int.Parse(list[9]);
+        }
+    }
+}
---
+++ Assets/RummyScript/Baccarat/UI/existingRoomPanelController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class existingRoomPanelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject tableContainer;
+    public UIBRoomItem originItem;
+
+    public List<UIBRoomItem> roomList = new List<UIBRoomItem>();
+    public List<BaccaratRoomInfo> roomInfoList = new List<BaccaratRoomInfo>();
+    void Start()
+    {
+        BaccaratRoomInfo roomInfo = new BaccaratRoomInfo();
+        roomInfo.isPrivate = false;
+        roomInfo.tableName = "11111";
+        roomInfo.status = "";
+        roomInfo.minBet = 50;
+        roomInfo.maxBet = 42342;
+        roomInfo.totalPlayers = 10;
+        roomInfo.playersNum = 3;
+
+        AddNewRoom(roomInfo);
+
+        BaccaratRoomInfo roomInfo1 = new BaccaratRoomInfo();
+        roomInfo1.isPrivate = true;
+        roomInfo1.tableName = "22222";
+        roomInfo1.status = "";
+        roomInfo1.minBet = 50;
+        roomInfo1.maxBet = 42342;
+        roomInfo1.totalPlayers = 10;
+        roomInfo1.playersNum = 3;
+
+        AddNewRoom(roomInfo1);
+
+
+        BaccaratRoomInfo roomInfo2 = new BaccaratRoomInfo();
+        roomInfo2.isPrivate = true;
+        roomInfo2.tableName = "33333";
+        roomInfo2.status = "";
+        roomInfo2.minBet = 50;
+        roomInfo2.maxBet = 42342;
+        roomInfo2.totalPlayers = 10;
+        roomInfo2.playersNum = 3;
+
+        AddNewRoom(roomInfo2);
+    }
+
+    internal void ShowRoomList(int id)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void AddNewRoom(BaccaratRoomInfo room)
+    {
+        roomInfoList.Add(room);        
+        originItem = Instantiate(originItem, tableContainer.transform);
+        originItem.SetMe(room);        
+        roomList.Add(originItem);
+    }
+    
+
+}
