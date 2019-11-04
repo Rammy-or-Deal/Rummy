7b743d52ca609b9e3a82b4e8624d87c7352c7ea6
akioUnity <akioUnity@gmail.com>
2019-11-04 23:45:53 +0700

    fix issue

--- Assets/RummyScript/Baccarat/UI/UIBCardBend.cs
+++ Assets/RummyScript/Baccarat/UI/UIBCardBend.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
-using Unity.UNetWeaver;
 using UnityEngine;
 
 public class UIBCardBend : MonoBehaviour
