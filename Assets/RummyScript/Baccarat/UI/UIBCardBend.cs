6e86eb41a6464fb4fa0c995d8ad6ed7380a3e8b1
akioUnity <akioUnity@gmail.com>
2019-11-04 14:22:30 +0700

    add card bend function

---
+++ Assets/RummyScript/Baccarat/UI/UIBCardBend.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.UNetWeaver;
+using UnityEngine;
+
+public class UIBCardBend : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 lastPoint;
+    public Transform[] bend;
+    public Transform[] card;
+    public int id;
+    public bool isClicked;
+    private float damping = 10;
+    void Start()
+    {
+        
+    }
+    
+    void Update () {
+        // Handle native touch events
+        foreach (Touch touch in Input.touches) {
+            HandleTouch(touch.fingerId,touch.position, touch.phase);
+        }
+
+        // Simulate touch events from mouse events
+        if (Input.touchCount == 0) {
+            if (Input.GetMouseButtonDown(0) ) {
+                HandleTouch(10, Input.mousePosition, TouchPhase.Began);
+            }
+            if (Input.GetMouseButton(0) ) {
+                HandleTouch(10, Input.mousePosition, TouchPhase.Moved);
+            }
+            if (Input.GetMouseButtonUp(0) ) {
+                HandleTouch(10, Input.mousePosition, TouchPhase.Ended);
+            }
+        }
+    }
+
+    private void HandleTouch(int touchFingerId, Vector3 touchPosition, TouchPhase touchPhase) {
+        switch (touchPhase) {
+            case TouchPhase.Began:
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(touchPosition), out hit, 100))
+                {
+                    UIBCardModel card= hit.collider.GetComponent<UIBCardModel>();
+                    if (card)
+                    {
+                        Debug.Log(hit.point+card.id.ToString());
+                        id = card.id;
+                        lastPoint = hit.point;
+                        isClicked = true;
+                    }
+                }
+                break;
+            case TouchPhase.Moved:
+                if (isClicked)
+                {
+                    if (Physics.Raycast(Camera.main.ScreenPointToRay(touchPosition), out hit, 100))
+                    {
+                        
+                        Vector3 relativePos = hit.point-lastPoint;
+                        float dis = relativePos.magnitude;
+                        if (dis<0.2)
+                            break;
+//                        Debug.Log(dis);
+                        bend[id].position = hit.point;
+//                        Debug.Log(hit.point);
+                        // the second argument, upwards, defaults to Vector3.up
+                        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+                        rotation *= Quaternion.Euler(0, 90, 0);
+                        bend[id].rotation = rotation;// Quaternion.Slerp(bend[id].rotation, rotation, Time.deltaTime * damping);
+                    }
+                }
+                break;
+            case TouchPhase.Ended:
+                if (isClicked)
+                {
+                    bend[id].position = new Vector3(2,0,0);
+                    bend[id].localRotation=new Quaternion(0,0,0,0);
+                    isClicked = false;
+                }
+                break;
+        }
+    }
+}
---
+++ Assets/RummyScript/Baccarat/UI/UIBCardModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBCardModel : MonoBehaviour
+{
+    public int id;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
