eb24fa4852f5a9741dc03d0d301bde7be664f1b1
akioUnity <akioUnity@gmail.com>
2019-11-05 14:31:16 +0700

    change image in selection

---
+++ Assets/RummyScript/Fortune13/UI/UIChangeCardDialog.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIChangeCardDialog : MonoBehaviour
+{
+    //cards
+    public FortuneCard[] frontCards;
+    public FortuneCard[] middleCards;
+    public FortuneCard[] backCards;
+    public FortuneCard[] myCards;
+    //
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
