d242a54ebf0ec31746ee88347000fbb091d5708d
akioUnity <akioUnity@gmail.com>
2019-11-09 13:36:02 +0700

    add center coin and UI

---
+++ Assets/RummyScript/Fortune13/UI/UIFCalcPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFCalcPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public FortuneCard[] cards;
+    public Image levelImg;
+    public Text coinText;
+    public Text cardText;
+    
+    void Start()
+    {
+        
+    }
+}
