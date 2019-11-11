ddfba1e88d3db4e6fabfc8c2d9858c750f6688f6
akioUnity <akioUnity@gmail.com>
2019-11-11 16:16:18 +0700

    update ResultDlg

--- Assets/RummyScript/Fortune13/UI/UIResultDialog.cs
+++ Assets/RummyScript/Fortune13/UI/UIResultDialog.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIResultDialog : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public UIFResultPlayer[] players;
+    public Text frontTxt;
+    public Text middleTxt;
+    public Text backTxt;
+    public Text totalTxt;
+    public Text tableTaxTxt;
+    
     void Start()
     {
         
