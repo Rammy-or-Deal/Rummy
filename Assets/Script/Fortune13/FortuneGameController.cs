452700d6f87190062b38a48b6ca838c4c9b6b0c2
akioUnity <akioUnity@gmail.com>
2019-11-30 08:17:18 +0700

    rummy chat

--- Assets/Script/Fortune13/FortuneGameController.cs
+++ Assets/Script/Fortune13/FortuneGameController.cs
@@ -6,12 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class FortuneGameController : GameController
 {
-    private void Start()
-    {
-        UIController.Inst.userInfoPanel.gameObject.SetActive(false);
-        UIController.Inst.moneyPanel.gameObject.SetActive(false);
-    }
-
     public override void SendMessage(int messageId, Player p = null)
     {
         FortuneMessageMgr.Inst.OnMessageArrived(messageId, p);
