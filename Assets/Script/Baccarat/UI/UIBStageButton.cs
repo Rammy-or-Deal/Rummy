a309d2b6a56a33618e9db9be52a34f22a0c8a8e1
blockfan@outlook.com <blockfan@outlook.com>
2019-12-14 01:51:25 +0800

    Change 2_Baccarat UI

--- Assets/Script/Baccarat/UI/UIBStageButton.cs
+++ Assets/Script/Baccarat/UI/UIBStageButton.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIBStageButton : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject coverImage;
+    public Image coverImage;
     public enumGameTier m_gameTier;
     public bool isOnlyView;
     public UIBStageButtonContainer parent;
+    private bool _isSelected = false;
     public bool isSelected{
-        get{ return coverImage.activeSelf;}
-        set{ coverImage.SetActive(value);}
+        get{ return _isSelected;}
+        set{ 
+            _isSelected = value;
+            string imgPath = "";
+            if(_isSelected)
+            {
+                imgPath = "baccarat/LVL/btn0";                
+            }
+            else
+            {
+                imgPath = "baccarat/LVL/btn1";
+            }
+            coverImage.sprite = Resources.Load<Sprite>(imgPath);
+            Debug.Log(imgPath);
+            //coverImage.SetActive(value);
+        }
     }
     void Start()
     {
