56c8f6cb72545e55aa3b0d328c5e9727b4029157
akioUnity <akioUnity@gmail.com>
2019-12-20 17:13:53 +0700

    DebugMgr reset cached git

---
+++ Assets/Script/GameCommon/MgrClasses/DebugMgr.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMgr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    List<enumLogLevel> avail_logs;
+
+    private void Awake() {
+        avail_logs = new List<enumLogLevel>();
+
+        if (constantContainer.buildMethod == enumBuildMethod.Development_Debug || constantContainer.buildMethod == enumBuildMethod.Product_Debug)
+        {
+            //avail_logs.Add(enumLogLevel.initLog);
+            avail_logs.Add(enumLogLevel.RoomLog);  //amg  
+            // avail_logs.Add(enumLogLevel.MeLog);
+            // avail_logs.Add(enumLogLevel.BotLog);
+            // avail_logs.Add(enumLogLevel.ControllerMessage);
+            // avail_logs.Add(enumLogLevel.RummySeatMgrLog);
+            // avail_logs.Add(enumLogLevel.RummyCardMgrLog);
+            //avail_logs.Add(enumLogLevel.BaccaratLogicLog);            
+//            avail_logs.Add(enumLogLevel.FortuneLuckyLog);
+        }
+    }
+    
+    public void Log(string log, enumLogLevel level = enumLogLevel.initLog)
+    {
+        enumGameType gameType = GameMgr.Inst.m_gameType;
+        if (!avail_logs.Contains(level)) return;
+        Debug.Log(gameType + ": " + log);
+    }
+
+}
