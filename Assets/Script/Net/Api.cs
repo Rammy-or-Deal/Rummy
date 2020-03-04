64ccac97f1dc707fd69b6e213c04b78187db6bfc
akioUnity <akioUnity@gmail.com>
2020-03-05 02:36:54 +0700

    test Get api

--- Assets/Script/Net/Api.cs
+++ Assets/Script/Net/Api.cs
@@ -13,121 +13,117 @@ public class Api : MonoBehaviour
     private RequestHelper currentRequest;
 
     void Awake()
-    {       
+    {
         Inst = this;
     }
 
     void Start()
     {
-        
+        Get();
     }
-	
-	public void Get(){
-		RequestHelper requestOptions = null;
 
-		RestClient.GetArray<Post>(basePath + "/posts").Then(res => {
-			this.LogMessage("Posts", JsonHelper.ArrayToJsonString<Post>(res, true));
-			return RestClient.GetArray<Todo>(basePath + "/todos");
-		}).Then(res => {
-			this.LogMessage("Todos", JsonHelper.ArrayToJsonString<Todo>(res, true));
-			return RestClient.GetArray<User>(basePath + "/users");
-		}).Then(res => {
-			this.LogMessage("Users", JsonHelper.ArrayToJsonString<User>(res, true));
+    public void Get()
+    {
+        RequestHelper requestOptions = null;
 
-			// We can add specific options and override default headers for a request
-			requestOptions = new RequestHelper { 
-				Uri = basePath + "/photos",
-				Headers = new Dictionary<string, string> {
-					{ "Authorization", "Other token..." }
-				},
-				EnableDebug = true
-			};
-			return RestClient.GetArray<Photo>(requestOptions);
-		}).Then(res => {
-			this.LogMessage("Header", requestOptions.GetHeader("Authorization"));
-		}).Catch(err => this.LogMessage("Error", err.Message));
-	}
+        RestClient.GetArray<UserInfoModel>(basePath + "/users/udid/111")
+            .Then(res => { this.LogMessage(JsonHelper.ArrayToJsonString(res, true)); })
+            .Catch(err => this.LogMessage(err.Message));
+    }
 
-	public void Post(){
+    public void Post()
+    {
+        currentRequest = new RequestHelper
+        {
+            Uri = basePath + "/posts",
+            Body = new Post
+            {
+                title = "foo",
+                body = "bar",
+                userId = 1
+            }
+        };
+        RestClient.Post<Post>(currentRequest)
+            .Then(res => this.LogMessage(JsonUtility.ToJson(res, true)))
+            .Catch(err => this.LogMessage(err.Message));
+    }
 
-		currentRequest = new RequestHelper {
-			Uri = basePath + "/posts",
-			Body = new Post {
-				title = "foo",
-				body = "bar",
-				userId = 1
-			}
-		};
-		RestClient.Post<Post>(currentRequest)
-		.Then(res => this.LogMessage("Success", JsonUtility.ToJson(res, true)))
-		.Catch(err => this.LogMessage("Error", err.Message));
-	}
+    public void Put()
+    {
+        currentRequest = new RequestHelper
+        {
+            Uri = basePath + "/posts/1",
+            Body = new Post
+            {
+                title = "foo",
+                body = "bar",
+                userId = 1
+            },
+            Retries = 5,
+            RetrySecondsDelay = 1,
+            RetryCallback = (err, retries) =>
+            {
+                Debug.Log(string.Format("Retry #{0} Status {1}\nError: {2}", retries, err.StatusCode, err));
+            }
+        };
+        RestClient.Put<Post>(currentRequest, (err, res, body) =>
+        {
+            if (err != null)
+            {
+                this.LogMessage(err.Message);
+            }
+            else
+            {
+                this.LogMessage(JsonUtility.ToJson(body, true));
+            }
+        });
+    }
 
-	public void Put(){
+    public void Delete()
+    {
+        RestClient.Delete(basePath + "/posts/1", (err, res) =>
+        {
+            if (err != null)
+            {
+                this.LogMessage(err.Message);
+            }
+            else
+            {
+                this.LogMessage("Status: " + res.StatusCode.ToString());
+            }
+        });
+    }
 
-		currentRequest = new RequestHelper {
-			Uri = basePath + "/posts/1", 
-			Body = new Post {
-				title = "foo",
-				body = "bar",
-				userId = 1
-				
-			},
-			Retries = 5,
-			RetrySecondsDelay = 1,
-			RetryCallback = (err, retries) => {
-				Debug.Log (string.Format ("Retry #{0} Status {1}\nError: {2}", retries, err.StatusCode, err));
-			}
-		};
-		RestClient.Put<Post>(currentRequest, (err, res, body) => {
-			if (err != null){
-				this.LogMessage("Error", err.Message);
-			}
-			else {
-				this.LogMessage("Success", JsonUtility.ToJson(body, true));
-			}
-		});
-	}
+    public void AbortRequest()
+    {
+        if (currentRequest != null)
+        {
+            currentRequest.Abort();
+            currentRequest = null;
+        }
+    }
 
-	public void Delete(){
+    public void DownloadFile()
+    {
+        var fileUrl = "https://raw.githubusercontent.com/IonDen/ion.sound/master/sounds/bell_ring.ogg";
+        var fileType = AudioType.OGGVORBIS;
 
-		RestClient.Delete(basePath + "/posts/1", (err, res) => {
-			if (err != null){
-				this.LogMessage("Error", err.Message);
-			}
-			else {
-				this.LogMessage("Success", "Status: " + res.StatusCode.ToString());
-			}
-		});
-	}
+        RestClient.Get(new RequestHelper
+        {
+            Uri = fileUrl,
+            DownloadHandler = new DownloadHandlerAudioClip(fileUrl, fileType)
+        }).Then(res =>
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.clip = ((DownloadHandlerAudioClip) res.Request.downloadHandler).audioClip;
+            audio.Play();
+        }).Catch(err => { this.LogMessage(err.Message); });
+    }
 
-	public void AbortRequest(){
-		if (currentRequest != null) {
-			currentRequest.Abort();
-			currentRequest = null;
-		}
-	}
-
-	public void DownloadFile(){
-
-		var fileUrl = "https://raw.githubusercontent.com/IonDen/ion.sound/master/sounds/bell_ring.ogg";
-		var fileType = AudioType.OGGVORBIS;
-
-		RestClient.Get(new RequestHelper {
-			Uri = fileUrl,
-			DownloadHandler = new DownloadHandlerAudioClip(fileUrl, fileType)
-		}).Then(res => {
-			AudioSource audio = GetComponent<AudioSource>();
-			audio.clip = ((DownloadHandlerAudioClip)res.Request.downloadHandler).audioClip;
-			audio.Play();
-		}).Catch(err => {
-			this.LogMessage("Error", err.Message);
-		});
-	}
-	
-	public void LogMessage(string message) {
+    public void LogMessage(string message)
+    {
 #if UNITY_EDITOR
-		Debug.Log(message);
+        Debug.Log(message);
 #endif
-	}
+    }
 }
