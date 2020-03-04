957a5108d9f93593b7e86a01f19aa2b82dde54a3
akioUnity <akioUnity@gmail.com>
2020-03-04 15:56:58 +0700

    remove intro scene

---
+++ Assets/Script/Main/LobbyController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyController : MonoBehaviour
+{
+    public Transform userInfoPanel;
+    public Transform moneyPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PunController.Inst.Login();
+        UIController.Inst.userInfoPanel.gameObject.SetActive(true);
+        UIController.Inst.moneyPanel.gameObject.SetActive(true);
+        UIController.Inst.userInfoPanel.transform.position = userInfoPanel.position;
+        UIController.Inst.moneyPanel.transform.position = moneyPanel.position;
+        UIController.Inst.userInfoPanel.transform.localScale = userInfoPanel.localScale;
+        UIController.Inst.moneyPanel.transform.localScale = moneyPanel.localScale;
+    }
+
+    public void OnClickLami()
+    {
+        //LoadGameScene("2_Lami");
+        GameMgr.Inst.LoadGameScene2(enumGameType.Lami);
+    }
+
+    public void OnClickBaccarat()
+    {
+        //LoadGameScene("2_Baccarat");
+        GameMgr.Inst.LoadGameScene2(enumGameType.Baccarat);
+        //PunController.Inst.CreateOrJoinBaccaratRoom();
+    }
+
+    public void OnClickFortune()
+    {
+        //LoadGameScene("2_Fortune13");
+        GameMgr.Inst.LoadGameScene2(enumGameType.Fortune13);
+    }
+
+    public void OnClikBack()
+    {
+        SceneManager.LoadScene("1_Title");
+    }
+    public void OnClickLobbyBottomBtn(int type)
+    {
+        Debug.Log("Lobby bottom click");
+
+        switch (type)
+        {
+            case 0:
+                UIController.Inst.eventDlg.gameObject.SetActive(true);
+                break;
+            case 1:
+                UIController.Inst.challengeDlg.gameObject.SetActive(true);
+                break;
+            case 2:
+                UIController.Inst.chatDlg.gameObject.SetActive(true);
+                break;
+            case 4:
+                UIController.Inst.rewardDlg.gameObject.SetActive(true);
+                break;
+            case 5:
+                UIController.Inst.emailDlg.gameObject.SetActive(true);
+                break;
+            case 6:
+                UIController.Inst.settingDlg.gameObject.SetActive(true);
+                break;
+            case 7:
+                UIController.Inst.spinDlg.gameObject.SetActive(true);
+                break;
+            case 8:
+                UIController.Inst.shopDlg.gameObject.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void OnClickUserPanel()
+    {
+        UIController.Inst.userInfoMenu.gameObject.SetActive(true);
+    }
+
+    public void OnClickMoneyPanel()
+    {
+        UIController.Inst.shopDlg.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Script/Net/DataController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using RummyScript.Model;
+using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
+
+public class DataController : MonoBehaviour
+{
+    public static DataController Inst;
+   
+    public FrameItemModel frameItem;
+    public FriendItemModel friendItem;
+    public GiftItemModel giftItem;
+    public LoginDayItemModel loginDayItem;
+    public SysItemModel sysItem;
+    public SysShopItemModel sysShopItem;
+    public SysSkillModel sysSkill;
+    public TierInfoModel tierInfo;
+    public UserInfoModel userInfo;
+    public SysExchangeItemModel sysExchangeItem;    
+    public SettingModel setting;
+    public Sprite facebookSprite;
+
+    void Awake()
+    {
+        if (Inst)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+            
+        Inst = this;
+        
+        setting = new SettingModel();
+        frameItem = new FrameItemModel();
+        friendItem = new FriendItemModel();
+        giftItem = new GiftItemModel();
+        loginDayItem = new LoginDayItemModel();
+        sysItem = new SysItemModel();
+        sysShopItem = new SysShopItemModel();
+        sysSkill = new SysSkillModel();
+        tierInfo = new TierInfoModel();
+        tierInfo.Init();
+        userInfo = new UserInfoModel();
+        userInfo.Init("User");
+        sysExchangeItem = new SysExchangeItemModel();
+        DontDestroyOnLoad(this.gameObject);
+        userInfo.udid=SystemInfo.deviceUniqueIdentifier;
+        Debug.Log(userInfo.udid);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (FB.IsLoggedIn)
+        {
+            GetNameAndPicture();
+        }
+    }
+    
+    public void GetNameAndPicture()
+    {
+        userInfo.facebookId = AccessToken.CurrentAccessToken.UserId;
+        userInfo.pic = userInfo.facebookId;
+        StartCoroutine(getFBPicture(userInfo.facebookId));
+        FB.API("me?fields=name", Facebook.Unity.HttpMethod.GET, delegate (IGraphResult result)
+        {
+            if (result.ResultDictionary != null)
+            {
+                foreach (string key in result.ResultDictionary.Keys)
+                {
+                    Debug.Log(key + " : " + result.ResultDictionary[key]);
+                }
+                userInfo.name = result.ResultDictionary["name"].ToString();
+                ChatMgr.Inst.chatClient.UserId = userInfo.name;
+                UIController.Inst.userInfoPanel.UpdateValue();
+            }
+        });
+    }
+
+    public void GetFBPicture(string facebookId, Sprite pic = null)
+    {
+        StartCoroutine(getFBPicture(facebookId, pic));
+
+    }
+
+    public IEnumerator getFBPicture(string facebookId,Sprite pic=null)
+    {
+        var www = new WWW("http://graph.facebook.com/" + facebookId +
+                          "/picture?width=250&height=250&type=square&redirect=true");
+        Debug.Log("http://graph.facebook.com/" + facebookId +
+                  "/picture?width=250&height=250&type=normal&redirect=true" + "\t" + www.error);
+        yield return www;
+
+        if (www.isDone)
+        {
+            Debug.Log("waiting" + www.bytesDownloaded);
+            Sprite sprite=Sprite.Create(www.texture,new Rect(0,0, www.texture.width, www.texture.width), new Vector2());
+            if (pic)
+                pic = sprite;
+            else
+            {
+                userInfo.sprite = sprite;
+                UIController.Inst.userInfoPanel.UpdateValue();
+                Debug.Log("facebook pic updated");
+            }
+        }
+    }
+}
